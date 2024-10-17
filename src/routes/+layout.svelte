8b12abc5bac01bcc65bd6115bd9b6e64698d2d77
Robert Gleason <l2ob122@gmail.com>
2024-10-16 18:22:52 -0700

    got the google fonts needs, and set up basic styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<h1>This is from the layout</h1>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<script></script>
+
+<h1>Landing Page</h1>
