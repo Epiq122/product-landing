c4809d6fc6309a8a356aae7b216a239741ec2f10
Robert Gleason <l2ob122@gmail.com>
2024-10-17 19:40:33 -0700

    faq-section: added the basic html layout

---
+++ src/lib/components/FaqSection.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { FaqItem, Button } from '$components';
+	import twitterIcon from '$assets/icons/twitter.webp';
+</script>
+
+<section class="faq-section">
+	<h2 class="mb-l">Frequently Asked Questions</h2>
+	<div class="faq-container">
+		<FaqItem />
+	</div>
+	<div class="additional-info">
+		<Button class="mt-s">Buy Now</Button>
+		<p class="mt-m">Any other questions?</p>
+		<a href="https://x.com">
+			<p>Hit me up on twitter <img src={twitterIcon} alt="twitter" class="twitter-icon" /></p>
+		</a>
+	</div>
+</section>
